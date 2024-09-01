4ced31fb47072c061ca79cc2663c5d0a5fed0006
Seokwoo <127665187+StoneCAU@users.noreply.github.com>
2024-09-01 22:07:31 +0900

    [Silver V] Title: 항해, Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/32200. 항해/항해.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(void) {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, x, y;
+	int t;
+	int cnt = 0; // 끼니 수는 최대가 되야함
+	int trash = 0; // 샌드위치 조각이 최소가 되야함
+
+	cin >> n >> x >> y;
+
+	for (int i = 0; i < n; i++)
+	{
+		int s;
+		cin >> s;
+
+		int max_meal = s / x;
+		int leftover = s % x;
+		int gap = max_meal*y - max_meal*x;
+		cnt += max_meal;
+
+		if (s < x) trash += s;
+		else if (gap < leftover)
+		{
+			int X = x;
+
+			while (max_meal == s / X && X <= y) X++;
+
+			trash += s % (X - 1);
+		}
+			
+
+			
+	}
+
+
+	cout << cnt << "\n" << trash;
+
+	return 0;
+}
+
+
