cbb66cb61aa7553c0fa0836f7c853c9a02c61a32
Seokwoo <127665187+StoneCAU@users.noreply.github.com>
2025-01-20 01:03:17 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.23 ms, Memory: 3.67 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int count(int n) {
+    int num = 0;
+    
+    for (int i=1;i<=n;i++) {
+        if (n % i == 0) num++;
+    }
+    
+    return num;
+}
+
+int solution(int left, int right) {
+    int answer = 0;
+    
+    for (int i=left;i<=right;i++) {
+        if (count(i) % 2 == 0) answer += i;
+        else answer -= i;
+    }
+    
+    return answer;
+}
