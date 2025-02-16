f553c5a9ac0ec7bc55c51ab2fb8727f3ef1c8319
Seokwoo <127665187+StoneCAU@users.noreply.github.com>
2025-02-16 11:03:17 +0900

    [Silver II] Title: N과 M (9), Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+int visited[9];
+vector<int> num;
+
+void dfs(int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	int prev = 0;
+
+	for (int i = 0; i < n; i++) {
+		if (!visited[i] && prev != num[i]) {
+			visited[i] = 1;
+			arr[cnt] = num[i];
+			prev = num[i];
+			dfs(cnt + 1);
+			visited[i] = 0;
+		}
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		int a;
+		cin >> a;
+		num.push_back(a);
+	}
+
+	sort(num.begin(), num.end());
+
+	dfs(0);
+}
