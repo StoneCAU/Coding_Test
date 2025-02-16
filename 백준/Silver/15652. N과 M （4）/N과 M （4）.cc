ca115c3f49811581bda58ad4cf67bdd3b2ed9cac
Seokwoo <127665187+StoneCAU@users.noreply.github.com>
2025-02-16 10:34:51 +0900

    [Silver III] Title: N과 M (4), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[9];
+int visited[9];
+
+void dfs(int num, int cnt) {
+	if (cnt == m) {
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = num; i <= n; i++) {
+			arr[cnt] = i;
+			dfs(i, cnt + 1);
+	}
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n >> m;
+
+	dfs(1, 0);
+}
