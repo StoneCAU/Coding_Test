1927d11eb66a3421bb707e2b6ec94ef70dfc0acc
Seokwoo <127665187+StoneCAU@users.noreply.github.com>
2025-07-10 17:52:08 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 52 ms, Memory: 10272 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,90 +1,60 @@
-#include <bits/stdc++.h>
-#include <algorithm>
-#include<unordered_map>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-int n, m;
-int arr[1002][1002];
-int dist[1002][1002][2];
-int dx[4] = {0,0,1,-1};
-int dy[4] = {1,-1,0,0};
+int n,m;
+int board[1001][1001];
+bool visited[1001][1001][2]; // 0: 벽 안부숨, 1: 벽 부숨
 
-queue<pair<pair<int, int>,int>> q;
-vector<pair<int, int>> v;
-vector<int> d;
+int dx[] = {1,-1,0,0};
+int dy[] = {0,0,1,-1};
 
-void bfs(int x, int y)
-{
-	q.push({ {x, y}, 0 });
+int bfs() {
+    queue<tuple<int,int,int,int>> q;
+    q.push({0,0,0,1});
+    visited[0][0][0] = true;
 
-	while (!q.empty())
-	{
-		auto cur = q.front();
+    while (!q.empty()) {
+        auto [xx, yy, st, cnt] = q.front();
+        q.pop();
 
-		if (cur.first.first == n && cur.first.second == m)
-		{
-			if (cur.second == 0)
-				cout << dist[n][m][0] + 1;
-			else cout << dist[n][m][1] + 1;
+        if (xx == n-1 && yy == m-1) return cnt;
 
-			return;
-		}
+        for (int i=0;i<4;i++) {
+            int nx = xx + dx[i];
+            int ny = yy + dy[i];
 
-		q.pop();
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (board[nx][ny] == 0) {
+                if (visited[nx][ny][st]) continue;
+                visited[nx][ny][st] = true;
+                q.push({nx, ny, st, cnt+1});
+            }
+            else if (board[nx][ny] == 1) {
+                if (visited[nx][ny][1]) continue;
+                if (st) continue;
+                visited[nx][ny][1] = true;
+                q.push({nx, ny, 1, cnt+1});
+            }
+        }
+    }
 
-		for (int i = 0; i < 4; i++)
-		{
-			int nx = cur.first.first + dx[i];
-			int ny = cur.first.second + dy[i];
-
-			if (nx <= 0 || nx > n || ny <= 0 || ny > m) continue;
-			if (!dist[nx][ny][1] && !cur.second && arr[nx][ny])
-			{				
-				dist[nx][ny][1] = dist[cur.first.first][cur.first.second][0] + 1;
-
-				q.push({ {nx, ny}, 1 });
-			}
-			else if (!dist[nx][ny][0] && !cur.second && !arr[nx][ny])
-			{
-				dist[nx][ny][0] = dist[cur.first.first][cur.first.second][0] + 1;
-
-				q.push({ {nx, ny}, 0 });
-			}
-			else if (!dist[nx][ny][1] && cur.second && !arr[nx][ny])
-			{
-				dist[nx][ny][1] = dist[cur.first.first][cur.first.second][1] + 1;
-
-				q.push({ {nx, ny}, 1 });
-			}
-		}
-	}
-	cout << "-1";
+    return -1;
 }
 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-int main(void) {
+    cin >> n >> m;
 
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    for (int i=0;i<n;i++) {
+        string str;
+        cin >> str;
+        for (int j=0;j<m;j++) {
+            board[i][j] = str[j] - '0';
+        }
+    }
 
-	int cnt = 0;
-
-	cin >> n >> m;
-
-	for (int i = 1; i <= n; i++)
-	{
-		string s;
-		cin >> s;
-
-		for (int j = 1; j <= m; j++)
-		{
-			arr[i][j] = s[j-1] - '0';
-
-		}
-	}
-
-	bfs(1, 1);
-
-	
-	
+    cout << bfs();
 }
