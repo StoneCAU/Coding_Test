95cfd81241358dc6621e3163f69deeb24d643f40
Seokwoo <127665187+StoneCAU@users.noreply.github.com>
2025-09-13 15:35:03 +0900

    [level 1] Title: 아픈 동물 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
+++ 프로그래머스/1/59036. 아픈 동물 찾기/아픈 동물 찾기.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-from ANIMAL_INS
-where INTAKE_CONDITION like 'Sick'
-order by ANIMAL_ID asc;
+select animal_id, name
+from animal_ins
+where intake_condition = 'Sick'
+order by animal_id
