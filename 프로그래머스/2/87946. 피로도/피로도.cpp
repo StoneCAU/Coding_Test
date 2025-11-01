37027e0056380221c48de635eaedb2727556bb26
Seokwoo <127665187+StoneCAU@users.noreply.github.com>
2025-11-01 09:24:26 +0900

    [level 2] Title: 피로도, Time: 0.44 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,43 +1,41 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
 
 int solution(int k, vector<vector<int>> dungeons) {
-    int answer = -1;
+    int answer = INT_MIN;
     
     vector<int> v;
-    
-    for (int i=0;i<dungeons.size();i++) {
+    for (int i=0;i<dungeons.size();++i) {
         v.push_back(i);
     }
     
     do {
-        vector<int> order;
+        int cur = k;
         
-        for(int i=0;i<v.size();i++) {
-            order.push_back(v[i]);
-        }
-        
-        int temp = k;
-        
-        for (int i=0;i<order.size();i++) {
-            int index = order[i];
-            int need = dungeons[index][0];
-            int use = dungeons[index][1];
+        // v[0]: 0번째 입장 던전 번호
+        for (int i=0;i<v.size();++i) {
+            int idx = v[i]; // 입장할 던전
+            int need = dungeons[idx][0];
+            int use = dungeons[idx][1];
             
-            if (temp < need) {
+            // 최소 피로도만큼 없을 경우 or 마지막 입장 경우
+            // update하고 다음 순서로 이동
+            if (cur - need < 0) {
                 answer = max(answer, i);
                 break;
             }
             
-            if (i == order.size() - 1) return order.size();
-
-            temp -= use; 
+            if (i == v.size() - 1) {
+                return (int) v.size();
+            }
+            
+            cur -= use;
         }
-    } while(next_permutation(v.begin(), v.end()));
+        
+        
+        
+    } while (next_permutation(v.begin(), v.end()));
     
     return answer;
 }
